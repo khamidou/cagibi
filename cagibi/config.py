6ef15cc619a8f931d747f00bd3315b00874f15b6
karim hamidou <khamidou@gmail.com>
2012-07-12 09:46:54 +0200

    added a file to handle a config file.

---
+++ cagibi/config.py
@@ -0,0 +1,9 @@
+import json
+
+def load_config(filename="config.json"):
+    """load a configuration file in json"""
+    return json.load(open(filename))
+
+def save_config(config, filename="config.json"):
+    """save a configuration file"""
+    json.dump(config, open(filename, 'w'))
