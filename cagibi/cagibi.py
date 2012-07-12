f19cccf6dfae2fa1144c04768699048bb46e0fb4
karim hamidou <khamidou@gmail.com>
2012-07-12 14:07:06 +0200

    client changes

--- cagibi/cagibi.py
+++ cagibi/cagibi.py
@@ -3,8 +3,11 @@
 from watch import FileWatcher
 from rsync import *
 from config import load_config
+import urllib2
+import json
 
 client_config = {}
+server_url = "http://localhost:8080/"
 
 def changesHandler(modified, added, removed):
     for file in modified:
@@ -12,8 +15,20 @@ def changesHandler(modified, added, removed):
         hashes = blockchecksums(fd)
         print hashes
 
+def checkout_changes():
+    """Checkout changes on the server"""
+    fd = urllib2.urlopen(server_url + "/folder")
+    data = json.load(fd)
+    print data
+
 if __name__ == "__main__":
     client_config = load_config("cagibi.json")
+    if "server" in client_config:
+        server_url = client_config["server"]
+        if server_url[-1] == '/':
+            server_url = server_url[0:-1]
+
     fw = FileWatcher()
     fw.addHandler(changesHandler)
+    checkout_changes()
     fw.watch()
