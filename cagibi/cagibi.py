76b43d4b351f241bc7a20f67d92ede02dfacfc7c
karim hamidou <khamidou@gmail.com>
2012-07-13 09:21:25 +0200

    retrieve new files from the server

--- cagibi/cagibi.py
+++ cagibi/cagibi.py
@@ -3,11 +3,13 @@
 from watch import FileWatcher
 from rsync import *
 from config import load_config
-import urllib2
+import urllib, urllib2
 import json
+import os
 
 client_config = {}
 server_url = "http://localhost:8080/"
+cagibi_folder = "."
 
 def changesHandler(modified, added, removed):
     for file in modified:
@@ -15,11 +17,28 @@ def changesHandler(modified, added, removed):
         hashes = blockchecksums(fd)
         print hashes
 
-def checkout_changes():
+def checkout_upstream_changes():
     """Checkout changes on the server"""
     fd = urllib2.urlopen(server_url + "/folder")
-    data = json.load(fd)
-    print data
+    server_files = json.load(fd)
+    fd.close()
+
+    local_files = load_config("files.json")
+
+    modified = False
+
+    for file in server_files:
+        if file not in local_files:
+            # Get it
+            local_files[file] = {}
+            local_files[file]["rev"] = server_files[file]["rev"]
+            url = "%s/files/%s/data" % (server_url, file) 
+            urllib.urlretrieve(url, os.path.join(cagibi_folder, file))
+            print "Retrieved %s" % file
+        else:
+            if server_files[file]["rev"] > local_files[file]["rev"]:
+                # Get it too, but using the rsync algo
+                pass
 
 if __name__ == "__main__":
     client_config = load_config("cagibi.json")
@@ -28,7 +47,10 @@ if __name__ == "__main__":
         if server_url[-1] == '/':
             server_url = server_url[0:-1]
 
+    if "folder" in client_config:
+        cagibi_folder = client_config["folder"]
+
     fw = FileWatcher()
     fw.addHandler(changesHandler)
-    checkout_changes()
+    checkout_upstream_changes()
     fw.watch()
