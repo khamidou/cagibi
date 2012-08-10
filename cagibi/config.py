43f6dad1787133424f8c6cf698b38737dd346a20
karim hamidou <khamidou@gmail.com>
2012-08-10 10:54:05 +0200

    refactored code

--- cagibi/config.py
+++ cagibi/config.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
+import sys
 import json
+import os
 
 def load_config(filename="config.json"):
     """load a configuration file in json"""
@@ -8,3 +10,13 @@ def load_config(filename="config.json"):
 def save_config(config, filename="config.json"):
     """save a configuration file"""
     json.dump(config, open(filename, 'w'))
+
+def get_config_path():
+    if os.name != "posix":
+        from win32com.shell import shellcon, shell
+        confpath = "{}\\cagibi\\cagibi.conf".format(shell.SHGetFolderPath(0, shellcon.CSIDL_APPDATA, 0, 0))
+    else:
+        confpath = "{}/.cagibi/cagibi.conf".format(os.path.expanduser("~"))
+
+    return confpath
+
